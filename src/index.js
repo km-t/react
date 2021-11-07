2f8fd4c947f4d699c070b7002d7701bb2663bd88
koba-masat <masatennis10140409@gmail.com>
2021-11-08 08:55:55 +0900

    状態を管理できるようになった

--- src/index.js
+++ src/index.js
@@ -2,9 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 class Square extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null,
+        };
+    }
+
     render() {
         return (
-            <button className="square" onClick={() => {alert('clicked'); }}>
+            <button className="square"
+                onClick={() => { this.setState({value: 'X'}); }}>
                 {this.props.value}
             </button>
         );
