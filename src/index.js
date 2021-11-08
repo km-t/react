e5d084deb4a3bffb3de68f5d5ee2b9da6237a827
koba-masat <masatennis10140409@gmail.com>
2021-11-08 13:33:31 +0900

    勝敗判定ができるようになった

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[b] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+
+}
+
 /**
  * 1マスを描画するメソッド
  * @param {*} props
@@ -40,6 +61,9 @@ class Board extends React.Component {
      */
     handleClick(i) {
         const squares = this.state.squares.slice();
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
         squares[i] = this.state.xIsNext ? 'X' : 'O';
         this.setState({
             squares: squares,
@@ -67,9 +91,16 @@ class Board extends React.Component {
      * @memberof Board
      */
     render() {
-        const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        const winner = calculateWinner(this.state.squares);
+        let status;
+        if (winner) {
+            status = 'Winner' + winner;
+        } else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
 
         return (
+
             <div>
                 <div className='status'>{status}</div>
                 <div className='board-row'>
